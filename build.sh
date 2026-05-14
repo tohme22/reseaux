2fd08bca8f758a5f6ce62638e8755ffdc78150ed
tohme22 <132168614+tohme22@users.noreply.github.com>
2026-05-14 17:38:22 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf public
-hugo
-
-ssh -p 65002 u254462768@195.35.38.198 "rm -rf /home/u254462768/domains/itmt-prof.com/public_html/reseaux/*"
-scp -rP 65002 public/* u254462768@195.35.38.198:public_html/reseaux/
